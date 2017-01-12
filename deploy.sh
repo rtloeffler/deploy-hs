6702094e85e1e440f8f7cf170fe4f44cfbb6a9a7
Ryan <rtloeffler@users.noreply.github.com>
2017-01-11 20:56:26 -0800

    elb addition

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 BRANCH=$1
 SERVER=`cat id`
 
+
 if [[ -z $1 ]]; then
 echo -e '\e[31myou must provide branch version: example:  "4.150.0"\e[0m'
 fi
@@ -14,5 +15,5 @@ fi
 
 if [[ $BRANCH != "" ]]; then
 ./single-deploy.sh $SERVER $BRANCH deploy && ./single-deploy.sh $SERVER $BRANCH intake && ./single-deploy.sh $SERVER $BRANCH process && ./single-deploy.sh $SERVER $BRANCH utils && ./single-deploy.sh $SERVER $BRANCH report && echo -e '\e[44mdeployed to ALL servers\e[0m'
-
+echo -e '\e[44mAttempting to add instance back to load balancer\e[0m' && ./single-deploy.sh $SERVER $BRANCH deploy reinstate
 fi
