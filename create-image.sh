8988dd4f24bc438a84f6bf5d360658bfbcb8c509
Ryan <rtloeffler@users.noreply.github.com>
2017-01-12 20:20:00 -0800

    added better functionality

--- create-image.sh
+++ create-image.sh
@@ -2,13 +2,14 @@
 INSTANCE_ID=`cat /var/tmp/aws-mon/instance-id`
 NAME=$1
 REINSTATE=$2
-ELB="amerigroup-web"
+ELB=`cat elb`
 
-
-if [[ $INSTANCE_ID != "" ]] && [[  $REINSTATE = "" ]]; then
+if [[ $INSTANCE_ID != "" ]] && [[  $REINSTATE != "reinstate" ]]; then
+	  echo -e '\e[44mSTARTING REGULAR MIGRATION \e[0m'
+	  cd /var/www/visitverify/www && php index.php db upgrade && echo -e '\e[44mREGULAR MIGRATION COMPLETED \e[0m'
       sudo aws elb deregister-instances-from-load-balancer --load-balancer-name $ELB --instances $INSTANCE_ID && echo -e '\e[44mREMOVED FROM ELB  SUCCESSFULLY\e[0m'
       sudo aws ec2 create-image --instance-id $INSTANCE_ID --name "$NAME" --region us-east-1 && echo -e '\e[44mIMAGE CREATED SUCCESSFULLY\e[0m'
-elif [[ $REINSTATE != "" ]];
+elif [[ $REINSTATE == "reinstate" ]];
 then
 	echo 'about to add instance back to load balancer'
       else
